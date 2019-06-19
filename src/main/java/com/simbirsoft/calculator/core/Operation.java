a2b612fc30d31f6f1c8422faf063f5e7f0eb4042
Nikita <nnigmat@gmail.com>
2019-06-19 19:31:50 +0300

    Multiply atan

--- src/main/java/com/simbirsoft/calculator/core/Operation.java
+++
@@ -1,6 +0,0 @@
-package com.simbirsoft.calculator.core;
-
-public interface Operation {
-
-    int execute(int a, int b);
-}
--- src/main/java/com/simbirsoft/calculator/exception/NotFoundOperationException.java
+++
@@ -1,8 +0,0 @@
-package com.simbirsoft.calculator.exception;
-
-public class NotFoundOperationException extends RuntimeException {
-
-    public NotFoundOperationException(String symbol) {
-        super(String.format("Operation [%s] not found", symbol));
-    }
-}
